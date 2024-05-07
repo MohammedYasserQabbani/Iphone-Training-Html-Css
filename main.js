cfb36d29d025c4df576e9003efd8f94eeae196de
MohammedyasserQabbani <mohammedyasser.qabbani@gmail.com>
2024-05-07 18:23:46 +0300

    (Iphone) => Training project using HTML & CSS

---
+++ main.js
@@ -0,0 +1,10 @@
+let img = document.querySelector('.img-pro');
+let container = document.querySelector('body');
+
+function phone_type(phone){
+    img.src = phone;
+}
+
+function color_background(color){
+    container.style.background = color;
+}
